5c6a8b5a01124d0cf5f841f5b7d5d107a96f006e
RRosas97 <ricardo.rosas@axity.com>
2024-08-22 20:37:56 -0600

    termino de las últimas 3 funcionalidades

--- swagger-api-demo/index.js
+++ swagger-api-demo/index.js
@@ -22,6 +22,69 @@ const users = [
   }
 ]
 
+const students = [
+  {
+    "id": 1,
+    "firstName": "Jose",
+    "lastName": "Montaño",
+    "age": 21,
+    "email": "Jose@axity.com",
+    "enrollmentNumber": "2024-001",
+    "courses": [
+      {
+        "courseId": 101,
+        "courseName": "Introduction to Programming",
+        "grade": "A"
+      },
+      {
+        "courseId": 102,
+        "courseName": "Data Structures"
+      }
+    ],
+    "status": "active"
+  },
+  {
+    "id": 2,
+    "firstName": "Ricardo",
+    "lastName": "Rosas",
+    "age": 24,
+    "email": "ricardo@axity.com",
+    "enrollmentNumber": "2024-002",
+    "courses": [
+      {
+        "courseId": 101,
+        "courseName": "Introduction to Programming",
+        "grade": "A"
+      },
+      {
+        "courseId": 102,
+        "courseName": "Data Structures"
+      }
+    ],
+    "status": "active"
+  },
+  {
+    "id": 3,
+    "firstName": "Sol",
+    "lastName": "Medrano",
+    "age": 22,
+    "email": "sol@axity.com",
+    "enrollmentNumber": "2024-001",
+    "courses": [
+      {
+        "courseId": 101,
+        "courseName": "Introduction to Programming",
+        "grade": "A"
+      },
+      {
+        "courseId": 102,
+        "courseName": "Data Structures"
+      }
+    ],
+    "status": "active"
+  }
+]
+
 /**
  * @swagger
  * /login:
@@ -59,14 +122,14 @@ app.post('/login', (req, res) => {
  * /items:
  *   get:
  *     summary: Retrieve a list of items
- *     Security:       
- *        - bearerAuth:[]      
+ *     security:       
+ *        - bearerAuth: []      
  *     responses:
  *       200:
  *         description: A list of items
  */
 app.get('/items',authenticateToken, (req, res) => {
-  res.status(200).json({ message: 'GET request - Retrieve items' });
+  res.status(200).json(students);
 });
 
 /**
@@ -74,14 +137,98 @@ app.get('/items',authenticateToken, (req, res) => {
  * /items:
  *   post:
  *     summary: Create a new item
- *     Security:
- *       - bearerAuth:[] 
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 description: "The student's first name"
+ *               lastName:
+ *                 type: string
+ *                 description: "The student's last name"
+ *               age:
+ *                 type: integer
+ *                 description: "The student's age"
+ *               email:
+ *                 type: string
+ *                 description: "The student's email address"
+ *               enrollmentNumber:
+ *                 type: string
+ *                 description: "The student's enrollment number"
+ *               courses:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     courseId:
+ *                       type: integer
+ *                     courseName:
+ *                       type: string
+ *                     grade:
+ *                       type: string
+ *               status:
+ *                 type: string
+ *                 description: "The student's status"
+ *                 example: "active"
  *     responses:
  *       201:
- *         description: Item created successfully
+ *         description: Student created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 firstName:
+ *                   type: string
+ *                 lastName:
+ *                   type: string
+ *                 age:
+ *                   type: integer
+ *                 email:
+ *                   type: string
+ *                 enrollmentNumber:
+ *                   type: string
+ *                 courses:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       courseId:
+ *                         type: integer
+ *                       courseName:
+ *                         type: string
+ *                       grade:
+ *                         type: string
+ *                 status:
+ *                   type: string
  */
+ 
 app.post('/items',authenticateToken, (req, res) => {
-  res.status(201).json({ message: 'POST request - Create item' });
+  const {firstName, lastName, age, email, enrollmentNumber, courses,status} = req.body;
+  const studentObject = {
+    id: students.length + 1
+    ,firstName, 
+    lastName,
+    age,
+    email,
+    enrollmentNumber,
+    courses,
+    status
+  }
+  students.push(studentObject)
+  res.status(201).json(
+    { message: 'Se ha agregado un estudiante',
+      nuevoEstudiante: studentObject
+    });
+    
 });
 
 /**
@@ -89,8 +236,8 @@ app.post('/items',authenticateToken, (req, res) => {
  * /items/{id}:
  *   get:
  *     summary: Retrieve a single item by ID
- *     Security:
- *       - bearerAuth:[]
+ *     security:
+ *       - bearerAuth: []
  *     parameters:
  *       - in: path
  *         name: id
@@ -103,29 +250,123 @@ app.post('/items',authenticateToken, (req, res) => {
  *         description: A single item
  */
 app.get('/items/:id', authenticateToken, (req, res) => {
-  res.status(200).json({ message: `GET request - Retrieve item with ID ${req.params.id}` });
+  const id =  parseInt(req.params.id) 
+  const student = students.find(student => student.id == id);
+
+   if(student){
+     res.status(200).json(student);
+   }
+   else{
+     res.status(404).json('Estudiante no encontrado ')
+   }
+
 });
 
 /**
+ * /**
  * @swagger
  * /items/{id}:
  *   put:
  *     summary: Update an item by ID
- *     Security:
- *       - bearerAuth:[]
+ *     security: 
+ *        - bearerAuth: []
  *     parameters:
  *       - in: path
  *         name: id
  *         required: true
- *         description: ID of the item to update
+ *         description: ID of the item to retrieve
  *         schema:
  *           type: string
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *                 description: "The student's first name"
+ *               lastName:
+ *                 type: string
+ *                 description: "The student's last name"
+ *               age:
+ *                 type: integer
+ *                 description: "The student's age"
+ *               email:
+ *                 type: string
+ *                 description: "The student's email address"
+ *               enrollmentNumber:
+ *                 type: string
+ *                 description: "The student's enrollment number"
+ *               courses:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     courseId:
+ *                       type: integer
+ *                     courseName:
+ *                       type: string
+ *                     grade:
+ *                       type: string
+ *               status:
+ *                 type: string
+ *                 description: "The student's status"
+ *                 example: "active"
  *     responses:
- *       200:
- *         description: Item updated successfully
+ *       201:
+ *         description: Student created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 firstName:
+ *                   type: string
+ *                 lastName:
+ *                   type: string
+ *                 age:
+ *                   type: integer
+ *                 email:
+ *                   type: string
+ *                 enrollmentNumber:
+ *                   type: string
+ *                 courses:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       courseId:
+ *                         type: integer
+ *                       courseName:
+ *                         type: string
+ *                       grade:
+ *                         type: string
+ *                 status:
+ *                   type: string
  */
+ 
 app.put('/items/:id', authenticateToken, (req, res) => {
-  res.status(200).json({ message: `PUT request - Update item with ID ${req.params.id}` });
+  const id =  parseInt(req.params.id) 
+  const student = students.find(student => student.id == id);
+ 
+  if(student){
+    student.firstName = (req.body.firstName)
+    student.lastName =(req.body.lastName)
+    student.age =(req.body.age)
+    student.email = req.body.email
+    student.enrollmentNumber = req.body.enrollmentNumber  
+    student.courses = req.body.courses
+    student.status = req.body.status  
+    res.status(200).json(student);
+  }
+  else{
+    res.status(404).json('Estudiante no encontrado')
+  }
+
 });
 
 /**
@@ -133,8 +374,6 @@ app.put('/items/:id', authenticateToken, (req, res) => {
  * /items/{id}:
  *   delete:
  *     summary: Delete an item by ID
- *     Security:
- *       - bearerAuth:[] 
  *     parameters:
  *       - in: path
  *         name: id
@@ -146,8 +385,20 @@ app.put('/items/:id', authenticateToken, (req, res) => {
  *       200:
  *         description: Item deleted successfully
  */
-app.delete('/items/:id', authenticateToken, (req, res) => {
-  res.status(200).json({ message: `DELETE request - Delete item with ID ${req.params.id}` });
+app.delete('/items/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const studentIndex = students.findIndex(student => student.id === id);
+  
+  if (studentIndex !== -1) {
+    const deletedStudent = students.splice(studentIndex, 1); 
+    res.status(200).json({
+      message: 'Estudiante eliminado con éxito',
+      student: deletedStudent[0]}
+    );
+  } else {
+    res.status(404).json('Estudiante no encontrado');
+  }
+  
 });
 
 // Setup Swagger
